ba53332801d4f2d436216670a116ac3fa6ee6a48
Владимир Ливинский <xDamneDx@Gmail.com>
2020-02-07 22:18:20 +0200

    In the process of passing 'Hello React' chapter

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,6 +2,11 @@
 
 console.log('App.js is running'); // JSX - JavaScript XML
 
-var template = React.createElement("h1", null, "Indecision App");
 var appRoot = document.getElementById('app');
+var app = {
+  title: "Indecision App",
+  subtitle: "Put Your Life in The Hands of The Computer",
+  options: ["One", "Two"]
+};
+var template = React.createElement("div", null, React.createElement("h1", null, app.title), app.subtitle && React.createElement("p", null, app.subtitle), React.createElement("p", null, app.options.length > 0 ? 'Here are your options:' : 'No options'), React.createElement("ol", null, React.createElement("li", null, "Item One"), React.createElement("li", null, "Item Tow")));
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,23 @@ console.log('App.js is running');
 
 // JSX - JavaScript XML
 
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
+
+const app = {
+    title: "Indecision App",
+    subtitle: "Put Your Life in The Hands of The Computer",
+    options: ["One", "Two"]
+}
+const template = (
+    <div>
+        <h1>{ app.title }</h1>
+        { app.subtitle && <p>{app.subtitle}</p> }
+        <p>{ app.options.length > 0 ? 'Here are your options:' : 'No options' }</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Tow</li>
+        </ol>
+    </div>
+);
 
 ReactDOM.render(template, appRoot);
