41600ee2462cbcffc1d6367dc1f11bc807c2039d
Владимир Ливинский <xDamneDx@Gmail.com>
2020-02-12 20:29:11 +0200

    Complete chapter 'Webpack'

---
+++ indecision-app/src/components/AddOption.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class AddOption extends React.Component {
+    state = {
+        error: undefined
+    }
+    handleAddOption = (e) => {
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
+
+        this.setState(() => ({ error }))  // same as error: error
+        e.target.elements.option.value = '';
+    };
+    render() {
+        return (
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" placeholder="Decision goes here..." name="option" />
+                    <button type="submit">Add Option</button>
+                </form>
+            </div>
+        );
+    };
+};
