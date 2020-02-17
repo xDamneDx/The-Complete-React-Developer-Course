460f5e966f623a9c2bd28b6e5e7b6a2221c5f3aa
Владимир Ливинский <xDamneDx@Gmail.com>
2020-02-17 18:12:24 +0200

    In the process of passing 'Redux' chapter

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -21,7 +21,7 @@ const setCount = ({ count }) => ({
     count
 });
 
-const store = createStore((state = { count: 0 }, action) => {
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -42,7 +42,9 @@ const store = createStore((state = { count: 0 }, action) => {
         default:
             return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
