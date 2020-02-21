7df14ee3e58d8c3e8b6747642a664053f828aa1f
Владимир Ливинский <xDamneDx@Gmail.com>
2020-02-21 17:23:51 +0200

    Complete chapter 'React with Redux'

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, removeExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { addExpense } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -13,13 +11,6 @@ const store = configureStore();
 store.dispatch(addExpense({ description: 'Water bill', amount: 1500 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 200, createdAt: 2001 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 10200 }));
-// store.dispatch(removeExpense(expense2.expense.id));
-// store.dispatch(setTextFilter('water'));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortByDate, sortByAmount } from '../actions/filters';
+import { DateRangePicker } from 'react-dates';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters';
 
-const ExpenseListFilters = (props) => (
-    <div>
-        <input type="text" onChange={(e) => {
-            props.dispatch(setTextFilter(e.target.value));
-        }} />
-        <select value={props.filters.sortBy} onChange={(e) => {
-            e.target.value === 'amount' ? props.dispatch(sortByAmount()) : props.dispatch(sortByDate());
-        }}>
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
-        </select>
-    </div>
-);
+class ExpenseListFilters extends React.Component {
+    state = {
+        calendarFocused: null,
+    };
+    onDatesChange = ({ startDate, endDate }) => {
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate));
+    }
+    onFocusChange = (calendarFocused) => {
+        this.setState(() => ({ calendarFocused }));
+    };
+    render() {
+        return (
+            <div>
+                <input type="text" onChange={(e) => {
+                    this.props.dispatch(setTextFilter(e.target.value));
+                }} />
+                <select value={this.props.filters.sortBy} onChange={(e) => {
+                    e.target.value === 'amount' ? this.props.dispatch(sortByAmount()) : this.props.dispatch(sortByDate());
+                }}>
+                    <option value="date">Date</option>
+                    <option value="amount">Amount</option>
+                </select>
+                <DateRangePicker 
+                    startDate={this.props.filters.startDate}
+                    endDate={this.props.filters.endDate}
+                    onDatesChange={this.onDatesChange}
+                    focusedInput={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    showClearDates={true}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
+            </div>
+        )
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
