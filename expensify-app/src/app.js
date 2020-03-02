900f5e89a90ada4c85c4a9b936ac22be75f0efff
Владимир Ливинский <xDamneDx@Gmail.com>
2020-03-02 18:53:29 +0200

    In the process of passing 'Deploying Your Apps'

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Water bill', amount: 1500 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 200, createdAt: 2001 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 10200 }));
 
 const jsx = (
     <Provider store={store}>
