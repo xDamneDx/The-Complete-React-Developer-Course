c89dc652768838a68df9235f35aa013fdd5f0ffb
Владимир Ливинский <xDamneDx@Gmail.com>
2020-03-07 22:26:39 +0200

    In the process of passing 'Firebase 101'

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -7,6 +7,8 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
+import './firebase/firebase';
+import './playground/promises';
 
 const store = configureStore();
 
